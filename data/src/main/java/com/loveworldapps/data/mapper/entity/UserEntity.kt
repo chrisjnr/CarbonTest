d2af72624912cc1f83e2f38a495fa2873e1837c5
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2021-03-27 18:08:31 +0100

    Added Db, Dao and entity classes

---
+++ data/src/main/java/com/loveworldapps/data/mapper/entity/UserEntity.kt
@@ -0,0 +1,21 @@
+package com.loveworldapps.data.mapper.entity
+
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Created by manuelchris-ogar on 27/03/2021.
+ */
+@Entity(tableName = "user_table")
+data class UserEntity(
+
+    @NonNull
+    @PrimaryKey
+    val id: String,
+    val email: String,
+    val title: String,
+    val picture: String,
+    val firstName: String,
+    val lastName: String
+)
