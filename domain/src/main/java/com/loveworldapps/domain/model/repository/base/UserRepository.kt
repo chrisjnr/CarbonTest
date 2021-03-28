0ff65e4a862a75f355904ca686f7f283bc09a1a8
Manuel Chris-Ogar <manuel.chris-ogar@konga.com>
2021-03-28 14:46:33 +0100

    User Repository Implementation

---
+++ domain/src/main/java/com/loveworldapps/domain/model/repository/base/UserRepository.kt
@@ -0,0 +1,26 @@
+package com.loveworldapps.domain.model.repository.base
+
+import androidx.paging.DataSource
+import com.loveworldapps.domain.model.User
+import com.loveworldapps.domain.model.UserResponse
+import io.reactivex.Observable
+import io.reactivex.Single
+
+/**
+ * Created by manuelchris-ogar on 27/03/2021.
+ */
+interface UserRepository {
+
+    fun loadUsers(limit: Int, page:Int =0): Observable<UserResponse?>?
+
+    fun addAllUsers(users: List<User>): Single<List<Long>>
+
+    fun addUser(user:User)
+
+    fun getUserNumber():Int
+
+    fun getAllUsers(): DataSource.Factory<Int, User>
+
+    fun searchForUser(query: String): List<User>
+
+}
